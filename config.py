71904084138c843cb9cbd2e30d7e816b5b1922c6
Florian Krause <fladd@web.de>
2015-02-21 00:11:04 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# The directory in wich the source markdown (.md) files are located 
+markdown_dir = "pages" 
+ 
+# Additional 'extras' to be used by the Python 'markdown2' package 
+extras = ['headers-id', 
+          'footnotes',
+          'fenced-code-blocks',
+          ]
+ 
+# Default settings (can be overwritten by meta data) 
+defaults = {"output_dir" : "~/public_html/stuff/",
+                   "url" : "http://www.example.com",
+                 "title" : "A StuffPages page", 
+               "favicon" : "http://www.example.com/favicon", 
+                 "style" : "styles/solarized_light.css", 
+           }
