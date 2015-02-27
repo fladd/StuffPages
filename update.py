ccf7cd5fee86a7370940cf4d69ab13d6b4b1ed91
Florian Krause <fladd@web.de>
2015-02-28 00:12:29 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-_version_ = 0.1.0
+_version_ = 0.2.0
 
 
 import os
@@ -15,37 +15,38 @@ from config import markdown_dir, extras, defaults
 
 
 for filename in glob(os.path.join(os.path.expanduser(markdown_dir), "*.md")):
+    _default = defaults.copy()
     root, ext = os.path.splitext(filename)
     body = markdown_path(filename, extras=["metadata"] + extras)
     meta = ""
     if hasattr(body, "metadata"):
         for m in body.metadata.keys():
             if m.lower() in ["output_dir", "url", "title", "favicon", "style"]:
-                defaults[m.lower()] = body.metadata[m]
+                _defaults[m.lower()] = body.metadata[m]
             else:
                 meta += '<meta name="{0}" content="{1}">\n'.format(
                     m.lower(), body.metadata[m])
 
-    htmldir = os.path.join(os.path.expanduser(defaults["output_dir"]),
+    htmldir = os.path.join(os.path.expanduser(_defaults["output_dir"]),
                            os.path.split(root)[-1])
     outfile = os.path.join(htmldir, "index.html")
     if not os.path.exists(outfile):
         os.makedirs(htmldir)
-    title = "<title>{0}</title>".format(defaults["title"])
-    if os.path.exists(os.path.expanduser(defaults["favicon"])):
-        shutil.copy(os.path.expanduser(defaults["favicon"]),
-                    os.path.expanduser(defaults["output_dir"]))
-        favicon_link = '<link rel="shortcut icon" href="{0}" type="image/vnd.microsoft.icon">'.format(os.path.join(default["url"], os.path.split(defauts["favicon"])[-1]))
-    elif defaults["favicon"].startswith("http"):
-        favicon_link = '<link rel="shortcut icon" href="{0}" type="image/vnd.microsoft.icon">'.format(defaults["favicon"])
+    title = "<title>{0}</title>".format(_defaults["title"])
+    if os.path.exists(os.path.expanduser(_defaults["favicon"])):
+        shutil.copy(os.path.expanduser(_defaults["favicon"]),
+                    os.path.expanduser(htmldir))
+        favicon_link = '<link rel="shortcut icon" href="{0}" type="image/vnd.microsoft.icon">'.format(os.path.join(_default["url"], os.path.split(defauts["favicon"])[-1]))
+    elif _defaults["favicon"].startswith("http"):
+        favicon_link = '<link rel="shortcut icon" href="{0}" type="image/vnd.microsoft.icon">'.format(_defaults["favicon"])
     else:
         favicon_link = ""
-    if os.path.exists(os.path.expanduser(defaults["style"])):
-        shutil.copy(os.path.expanduser(defaults["style"]),
-                    os.path.expanduser(defaults["output_dir"]))
-        css_link = '<link href="{0}" rel="stylesheet"></link>'.format(os.path.join(defaults["url"], os.path.split(defaults["style"])[-1]))
+    if os.path.exists(os.path.expanduser(_defaults["style"])):
+        shutil.copy(os.path.expanduser(_defaults["style"]),
+                    os.path.expanduser(htmldir))
+        css_link = '<link href="{0}" rel="stylesheet"></link>'.format(os.path.join(_defaults["url"], os.path.split(_defaults["style"])[-1]))
     elif defaults["style"].startswith("http"):
-        css_link = '<link href="{0}" rel="stylesheet"></link>'.format(defaults["style"])
+        css_link = '<link href="{0}" rel="stylesheet"></link>'.format(_defaults["style"])
     else:
         css_link = ""
     content = \
